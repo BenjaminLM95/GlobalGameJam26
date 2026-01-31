2dc98280b33c9f2da21e55c634e94a6462bcbe9c
JuliaCarew <crewjulia3@gmail.com>
2026-01-31 19:37:21 -0400

    number font thing

--- Assets/Julia_Scripts/Timer/Timer.cs
+++ Assets/Julia_Scripts/Timer/Timer.cs
@@ -2,16 +2,36 @@ using UnityEngine;
 using System.Collections;
 using TMPro;
 using System;
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections.Generic;
+using System.Globalization;
 
 public class Timer : MonoBehaviour
 {
     public float maxTime = 240f;
     private float currentTime;
     [SerializeField] private TextMeshProUGUI timerText;
-
+    [SerializeField] private Sprite[] numbers;
+    private Dictionary<int, Sprite> timerNums;
+    // if its bigger than 9
+    // divide by 10 - first digit assigned
+    // modulo 10 - second digit assigned 
     void Start()
     {
         currentTime = maxTime;
+        timerNums = new Dictionary<int, Sprite>()
+        {
+            {0, (Sprite)numbers.GetValue(0)},
+            {1, (Sprite)numbers.GetValue(1)},
+            {2, (Sprite)numbers.GetValue(2)},
+            {3, (Sprite)numbers.GetValue(3)},
+            {4, (Sprite)numbers.GetValue(4)},
+            {5, (Sprite)numbers.GetValue(5)},
+            {6, (Sprite)numbers.GetValue(6)},
+            {7, (Sprite)numbers.GetValue(7)},
+            {8, (Sprite)numbers.GetValue(8)},
+            {9, (Sprite)numbers.GetValue(9)},
+        };
     }
 
     void Update()
@@ -39,4 +59,6 @@ public class Timer : MonoBehaviour
         Debug.Log("Time's up!");
         // game over logic (call event, change state)
     }
+
+    // change sprite of text to number sprite
 }
